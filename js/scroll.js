0c9ab484200c7820f8051c44046b05d5a43d70b5
alex <lazywebstudying@gmail.com>
2019-08-07 19:00:43 +0300

    структура переделана под бєм, недоработаный дизайн!

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+// https://scotch.io/bar-talk/build-a-scroll-spy-navbar-solution-to-code-challenge-9
+const handleView = item => {
+    const linkEl = document.querySelector(`#link-${item}`);
+  
+    let offsetHeight = 0.6*(window.innerHeight)
+    inView.offset({
+      bottom:offsetHeight
+    });
+  
+    inView(`#${item}`)
+      .on("enter", () => linkEl.classList.add('is-active'))
+      .on("exit", el  => linkEl.classList.remove('is-active'))
+  };
+  
+  // Apply method on each DOM element
+  ["header", "provide", "projects", "link"].forEach(handleView);
